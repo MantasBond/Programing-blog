53caddc1d933a253bf48a06d0c669b20d3379f35
MantasBond <32520493+MantasBond@users.noreply.github.com>
2017-10-08 14:26:18 +0100

    Updated content div's and styling

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 	include 'includes/header.php';
 ?>
-<div id="content">
+<div id="error">
 <?php
 	if(strpos($url,'error=login')!==false){
 		echo "<p class='message'>Username/Password incorrect!</p>";
-	} else if(isset($_SESSION['id'])){
+	}
+
+	if(isset($_SESSION['id'])){
 	} else {
 		echo "<p class='message'>You are not logged in!</p>";
 	}
 ?>
+</div>
+<div class="content">
+	<h1 class="content_title">Title</h1>
+	<p class="content_p">Some text</p>
 </div>
 </body>
 </html>
