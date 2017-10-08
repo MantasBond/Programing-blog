d870af1e947da465cbbb40da5d34876c2bc0bf95
MantasBond <32520493+MantasBond@users.noreply.github.com>
2017-10-08 13:51:44 +0100

    Added div for content

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	include 'includes/header.php';
 ?>
-
+<div id="content">
 <?php
 	if(strpos($url,'error=login')!==false){
 		echo "<p class='message'>Username/Password incorrect!</p>";
@@ -10,5 +10,6 @@
 		echo "<p class='message'>You are not logged in!</p>";
 	}
 ?>
+</div>
 </body>
 </html>
