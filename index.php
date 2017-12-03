92d66032b71011156bc5e00cee80bbf48472ce98
MantasBond <32520493+MantasBond@users.noreply.github.com>
2017-12-03 16:58:57 +0000

    Form input amended
    auto complete = off

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 ?>
 <div id="login_menu">
 	<form id='loginForm' action='includes/login.inc.php' method='POST'>
-			<input id='login-uid' type='text' name='uid' placeholder='Username'>
-			<input id='login-pwd' type='password' name='pwd' placeholder='Password'>
+			<input id='login-uid' type='text' name='uid' placeholder='Username' autocomplete="off">
+			<input id='login-pwd' type='password' name='pwd' placeholder='Password' autocomplete="off">
 			<button id='login-submit' type='submit'>LOGIN</button>
 			<p class="login-message"></p>
 	</form>;
@@ -12,11 +12,11 @@
 
 <div id="reg_menu">
 	<form id='signup' action='includes/signup.inc.php' method='POST'>
-		<input id="sign-first" type='text' name='first' placeholder='First Name'>
-		<input id="sign-last" type='text' name='last' placeholder='Last Name'>
-		<input id="sign-uid" type='text' name='uid' placeholder='Username'>
-		<input id="sign-pwd" type='password' name='pwd' placeholder='Password'>
-		<input id="sign-repwd" type="password" name="repwd" placeholder='Repeat Pasword'>
+		<input id="sign-first" type='text' name='first' placeholder='First Name' autocomplete="off">
+		<input id="sign-last" type='text' name='last' placeholder='Last Name' autocomplete="off">
+		<input id="sign-uid" type='text' name='uid' placeholder='Username' autocomplete="off">
+		<input id="sign-pwd" type='password' name='pwd' placeholder='Password' autocomplete="off">
+		<input id="sign-repwd" type="password" name="repwd" placeholder='Repeat Pasword' autocomplete="off">
 		<button id="sign-submit" type='submit'>SIGN UP</button>
 		<p class="sign-message"></p>
 	</form>";
